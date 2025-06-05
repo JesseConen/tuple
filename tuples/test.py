6402eac9e055a4cab9af953af8efb1c0e96302c7
JesseConen <jessekonen@hotmail.com>
2025-06-05 16:57:13 +0200

    Add files via upload

---
+++ tuples/test.py
@@ -0,0 +1,9 @@
+#Jesse took three small tests in school. He scored 72 in Math, 85 in English, and 90 in Science.
+#Question: Use a tuple to help us find JEsse’s total score and his average score (out of 100).
+
+scores = (72, 85, 90)
+
+math, english, science = scores
+
+print(math + english + science)
+print(math + english + science / 3)
